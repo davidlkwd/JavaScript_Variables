70ffec53726424035c3f6e2f636772ecc9a9599f
David Herrbach <dave@ihmrinc.com>
2021-02-01 18:08:32 -0800

    Initial Commit

--- app.js
+++ app.js
@@ -4,7 +4,26 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "David";
+let lastName = "Herrbach";
+var myAge = 41;
+
+console.log(firstName);
+console.log(lastName);
+console.log(myAge);
 
 // Exercise 2
+let fullNameLastName = firstName + " " + lastName;
+let templateLiteralFullName = `${firstName} ${lastName}`;
+
+console.log(fullNameLastName);
+console.log(templateLiteralFullName);
 
 // Exercise 3
+let fullName = "David Herrbach";
+let city = "Lakewood";
+let pastime = "Golf";
+
+let myStory = `Hello! My name is ${fullName}. I live in ${city}, California. I enjoy ${pastime} and learning to code.`;
+
+console.log(myStory);
